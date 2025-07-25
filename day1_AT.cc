4153c96d59cbee961c624100920e5220421b9ac6
Naman Porwal <lucario12028@gmail.com>
2025-07-25 21:58:23 +0530

    Update day1_AT.cc

--- day1_AT.cc
+++ day1_AT.cc
@@ -2,13 +2,19 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j]==target){
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
                     ans.push_back(i);
                     ans.push_back(j);
                 }
