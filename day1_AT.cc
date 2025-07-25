f8dfe9c2e370b34ca40dab24070e295dafaa685b
Naman Porwal <99230915+heyNam1@users.noreply.github.com>
2025-07-25 21:58:01 +0530

    Update day1_AT.cc
    1. Two Sum

--- day1_AT.cc
+++ day1_AT.cc
@@ -6,8 +6,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1; j<nums.size(); j++){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1; j<n; j++){
                 if(nums[i]+nums[j]==target){
                     ans.push_back(i);
                     ans.push_back(j);
