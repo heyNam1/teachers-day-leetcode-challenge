761e730b4480eb631507584c892e6771a2398179
Naman Porwal <lucario12028@gmail.com>
2025-07-25 21:46:26 +0530

    Create day1_bt.cc
    1295. Find Numbers with Even Number of Digits

---
+++ day1_bt.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count=0;
+        for(int n:nums){
+            if(to_string(n).length()%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
