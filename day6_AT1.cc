1c60037fd87d231e24bdf006df40d3be9d730ef2
Naman Porwal <lucario12028@gmail.com>
2025-07-31 00:03:30 +0530

    Create day6_AT1.cc
    53. Maximum Subarray

---
+++ day6_AT1.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        int currSum = nums[0];
+        int totalSum = nums[0];
+
+        for (int i = 1; i < n; i++)
+        {
+
+            currSum = max(nums[i],currSum+nums[i]);
+
+            totalSum = max(currSum,totalSum);
+        }
+
+        return totalSum;
+    }
+};
