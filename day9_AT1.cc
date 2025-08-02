5fef588d11692f7b2d9cdda9a5b5151363ca4bbc
Naman Porwal <lucario12028@gmail.com>
2025-08-02 22:26:49 +0530

    Create day9_AT1.cc
    112-A-Petya and strings

---
+++ day9_AT1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    // Convert both to lowercase
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+    }
+
+    if (a == b)
+        cout << 0;
+    else if (a < b)
+        cout << -1;
+    else
+        cout << 1;
+
+    return 0;
+}
